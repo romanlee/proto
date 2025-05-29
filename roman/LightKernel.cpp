47a786ca48a6f57c51256a39f10003cabdc81e08
romanlee <romanlee@physics.ucla.edu>
2025-05-28 17:03:38 -0700

    Cleaning things up

--- roman/LightKernel.cpp
+++ roman/LightKernel.cpp
@@ -9,11 +9,10 @@ using namespace Proto;
 PROTO_KERNEL_START
 void consToPrim_temp(Var<double,DIM+2>& W, 
                      const Var<double, DIM+2>& U,
-                     double gamma, int N)
+                     double gamma, int N_int)
 {
-  for (int j=0; j<N; j++){
-    // printf("j=%d\n", j); // SCREWING THINGS UP FOR SOME REASON??
-
+  // Heavy kernel loop
+  for (int j=0; j<N_int; j++){
     double rho = U(0);
     double v, v2=0.;
     W(0) = rho;
@@ -27,34 +26,48 @@ void consToPrim_temp(Var<double,DIM+2>& W,
       }
     
     W(DIM+1) = (U(DIM+1)-.5*rho*v2) * (gamma-1.);
-
   }
 }
 PROTO_KERNEL_END(consToPrim_temp, consToPrim)
 
 int main(){
 
-  PR_TIMER_SETFILE("roman/HeavyKernel/TIMINGS.txt")
+  PR_TIMER_SETFILE("roman/LightKernel/TIMINGS.txt")
 
   std::cout << "Default memory type: " << parseMemType(MEMTYPE_DEFAULT) << std::endl;
 
-  // Hyper params
+  // TODO make these hyper params
   const int nx = 512;
-  const int N = 100;
-
-  cout << "DIM" << DIM << "\n";
+  const int N_ext = 10; // Set >1 for "light kernel" test, else  set =1
+  const int N_int = 1; // Set > 1 for "heavy kernel" test, else set =1
 
   // Set up the input array
   Box srcBox = Box::Cube(nx);
   BoxData<double,DIM+2> U(srcBox,1);
-  BoxData<double,DIM+2> W;
+  BoxData<double,DIM+2> W(srcBox,1);
+
+  // Stencil<double> S = 0.5*Shift::X(-1) + 0.5*Shift::X(+1); // This means: S[D_{i}] = 0.5*D_{i-1} + 0.5*D_{i+1}
 
   const double gamma = 1.4;  
 
-  // Call heavy kernel
+  // Light kernel loop
   {
-    PR_TIME("heavy");
-    W = forall<double,DIM+2>(consToPrim,U,gamma, N);
+    PR_TIME("forall_zone");
+
+    for (int i=0; i<N_ext; i++){
+      // cout << "i: " << i << "\n";
+
+      // auto W = forall<double,DIM+2>(consToPrim,U,gamma,N_int);
+      // forallInPlace<double,DIM+2,MEMTYPE_DEFAULT,1,1>(consToPrim,W,U,gamma,N_int);
+      forallInPlace(consToPrim,W,U,gamma,N_int);
+
+      // Not sure this is necessary. This kernel seems to happen in serial no matter what
+      // cudaDeviceSynchronize(); 
+
+      // // TODO try interleaving a stencil
+      // X += S(W);
+    }
+
   }
 
   // Check the reuslts (from ForallTests.cpp)
@@ -70,7 +83,9 @@ int main(){
   // W2.copyTo(W2_host);
   // consToPrimCheck(U_host,W2_host,gamma,destBox);
 
-  PR_TIMER_REPORT();
+  // // Screws up Nsight systems except when...the PR_TIMER_SETFILE is in the same dir as
+  // // the .nsys-rep???
+  // PR_TIMER_REPORT();
 
   return 0;
 }
